c2128bc6cfa291618cb250367e41304a7c6c338b
Hristo Karchev <x_karchev@mail.bg>
2022-09-05 09:59:29 +0300

    update/header created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header/Header";
+
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hi</h1>
+    <div className="app">
+      <Header />
+      <h1>Hi</h1>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header_navigation'>
+                <span>Dashboard</span>
+                <span>Help</span>
+            </div>
+        </div>
+    )
+};
