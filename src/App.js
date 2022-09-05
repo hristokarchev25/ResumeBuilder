dcef71a57862812de2593483b10f84bb05081957
Hristo Karchev <x_karchev@mail.bg>
2022-09-05 10:47:01 +0300

    update/react-router-dom installed and added to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
-
-
+import Help from './components/Help/Help';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Hi</h1>
+      <Routes>
+        <Route path='/' exact element={<Dashboard />} />
+        <Route path='/help' exact element={<Help />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
     return (
         <div className='header'>
             <div className='header_navigation'>
-                <span>Dashboard</span>
-                <span>Help</span>
+                <Link style={{ textDecoration: 'none' }} to="/">
+                    <span>Dashboard</span>
+                </Link>
+                <Link style={{ textDecoration: 'none' }} to="/help">
+                    <span>Help</span>
+                </Link>
             </div>
         </div>
     )
