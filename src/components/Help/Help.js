14f4cf4d84a948bd51b3872284e35fd2e271ab5b
Hristo Karchev <x_karchev@mail.bg>
2022-09-05 11:06:07 +0300

    feature/help page created

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './Help.css';
+
 
 export default function Help() {
     return (
-        <div>Help</div>
+        <div class="help_info">
+            <h1>Resume Builder</h1>
+            <p>
+                Resume Builder is a simple app where users can add their
+                experience in order for them to find a new job.
+                By clicking the "Add New Experience" button the user can add new section to the page.
+                The section is editable and would display to the user if there are any language mistakes.
+            </p>
+        </div>
     )
 };
