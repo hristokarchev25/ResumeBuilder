8c7f272059883fc29c53055574fe854ae09ba46d
Hristo Karchev <x_karchev@mail.bg>
2022-09-08 23:10:57 +0300

    fix/ bubble popup error messages

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './Dashboard.css';
 import icon from '../../utils/iconList.png';
 
@@ -19,25 +19,35 @@ export default function Dashboard() {
 
 
     const [description, setDescription] = useState("");
-    /* const [visible, setVisible] = useState(false); */
+    const [text, setText] = useState("Include a valuable metric if possible. For example: \"Increased revenue by 20% within one month.\"");
 
     const descriptionHandler = (e) => {
         e.preventDefault();
-        if (description === "I've done many projects") {
+        console.log(description.substr(0, 20));
+        //let result = description.substr(0, 20);
+
+        if (description.includes("many") || description.includes("a lot")) {
             document.getElementById("bubble").style.visibility = "visible";
+            document.getElementById("description_input").style.background = "rgba(235, 87, 87, 0.66)";
+            ///result.style.background = "rgba(235, 87, 87, 0.66)";
         } else {
             document.getElementById("bubble").style.visibility = "hidden";
+            document.getElementById("description_input").style.background = "#FFFFFF"
         }
     }
 
+    const descriptionHoverHandler = () => {
+        setText("This is vague. Instead of “Managed projects for many clients”, say “Managed projects for 10 clients including BlueBank.”");
+    }
 
+    const descriptionOnLeaveHandler = () => {
+        setText("Include a valuable metric if possible. For example: \"Increased revenue by 20% within one month.\"");
+    }
 
-    /* function handleClick() {
-        setVisible(!visible);
-    } */
+    const checkHandler = () => {
+        document.getElementById("bubble").style.display = "none";
+    }
 
-
-    console.log(description);
 
     return (
         <>
@@ -47,50 +57,49 @@ export default function Dashboard() {
             <div className="page">
 
                 {experienceList.map((singleExperience, index) => (
-                    <div value={singleExperience} key={index} className="experience_wrapper">
+                    <>
+                        <div value={singleExperience} key={index} className="experience_wrapper">
 
-                        <div className="experience_header">
-                            Experience
-                            {experienceList.length > 1 && (
-                                <span onClick={() => handleExperienceRemove(index)}>X</span>
-                            )}
-                        </div>
-                        <div className="title" contentEditable="true" data-text="Title"></div>
-                        <div className="company_name" contentEditable="true" data-text="Company Name"></div>
-                        <div className="date_city">
-                            {/* <span className="spanOne" contentEditable="true" data-text="Date period"></span>
+                            <div className="experience_header">
+                                Experience
+                                {experienceList.length > 1 && (
+                                    <span onClick={() => handleExperienceRemove(index)}>X</span>
+                                )}
+                            </div>
+                            <div className="title" contentEditable="true" data-text="Title"></div>
+                            <div className="company_name" contentEditable="true" data-text="Company Name"></div>
+                            <div className="date_city">
+                                {/* <span className="spanOne" contentEditable="true" data-text="Date period"></span>
                             <span className='spanTwo' contentEditable="true" data-text="New York, NY"></span> */}
-                            <input type="text" placeholder='Date period' />
-                            <input type="text" placeholder='New York, NY' />
+                                <input type="text" placeholder='Date period' />
+                                <input type="text" placeholder='New York, NY' />
+                            </div>
+                            <div className="description" /* contentEditable="true" data-text="Company Description" value={description} onChange={(e) => { setDescription(e.target.value) }} */>
+                                <input id='description_input' placeholder='Company Description' value={description} onChange={(e) => { setDescription(e.target.value) }}
+                                    onBlur={descriptionHandler}
+                                    onMouseOver={descriptionHoverHandler}
+                                    onMouseLeave={descriptionOnLeaveHandler} />
+                            </div>
                         </div>
-                        <div className="description" /* contentEditable="true" data-text="Company Description" value={description} onChange={(e) => { setDescription(e.target.value) }} */>
-                            <input placeholder='Company Description' value={description} onChange={(e) => { setDescription(e.target.value) }} onBlur={descriptionHandler} />
-                        </div>
-                    </div>
-                ))}
-
-                <div id='bubble' className='bubble_wrapper'>
-                    <div className='bubble'>
-                        <div className='bubble_content'>
-                            <div className='bubble_header'>
-                                <div className='content_logo_wrapper'>
-                                    <img src={icon} />
-                                    <span>Content improvement</span>
-                                </div>
-                                <div className='ignore_wrapper'>
-                                    <input type="checkbox" />
-                                    <span>Ignore</span>
+                        <div id='bubble' className='bubble_wrapper'>
+                            <div className='bubble'>
+                                <div className='bubble_content'>
+                                    <div className='bubble_header'>
+                                        <div className='content_logo_wrapper'>
+                                            <img src={icon} alt='icon list' />
+                                            <span>Content improvement</span>
+                                        </div>
+                                        <div className='ignore_wrapper'>
+                                            <input type="checkbox" onClick={checkHandler} />
+                                            <span>Ignore</span>
+                                        </div>
+                                    </div>
+                                    <p>{text}</p>
                                 </div>
                             </div>
-                            <p>This is vague. Instead of “Managed projects for many clients”, say “Managed projects for 10 clients including BlueBank</p>
                         </div>
-                    </div>
-                </div>
-
-                {/* <div>
-                    <button onClick={handleClick}>Toggle visibilty</button>
-                    <textarea className={!visible && 'bubble_wrapper'}></textarea>
-                </div> */}
+                    </>
+                ))}
             </div>
         </>
     )
